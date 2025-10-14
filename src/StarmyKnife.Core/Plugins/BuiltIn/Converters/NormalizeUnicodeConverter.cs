b437f35a5668c275db4b07c36322210278ab14d3
Kohei Kakimoto <naige.1028@gmail.com>
2025-10-14 22:34:26 +0900

    Add converter to normalize unicode string

---
+++ src/StarmyKnife.Core/Plugins/BuiltIn/Converters/NormalizeUnicodeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarmyKnife.Core.Plugins.BuiltIn.Converters
+{
+    [StarmyKnifePlugin("Normalize Unicode")]
+    public class NormalizeUnicodeConverter : PluginBase, IConverter
+    {
+        public class ParameterKeys
+        {
+            public const string Form = "Form";
+        }
+
+        public PluginInvocationResult Convert(string input, PluginParameterCollection parameters)
+        {
+            var form = parameters[ParameterKeys.Form].GetValue<NormalizationForm>();
+            var output = input.Normalize(form);
+            return PluginInvocationResult.OfSuccess(output);
+        }
+
+        protected override void ConfigureParameters(PluginParametersConfiguration configuration)
+        {
+            configuration.AddListParameter<NormalizationForm>(ParameterKeys.Form, NormalizationForm.FormD);
+        }
+    }
+}
