811e606fafe94ebd58981a421826a20a1042c2bb
Kohei Kakimoto <naige.1028@gmail.com>
2025-10-14 22:35:03 +0900

    Add converters to convert japanese hiragana to/from katakana

---
+++ src/StarmyKnife.Core/Plugins/BuiltIn/Converters/HiraganaToKatakanaConverter.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarmyKnife.Core.Plugins.BuiltIn.Converters
+{
+    [StarmyKnifePlugin("Hiragana to Katakana")]
+    public class HiraganaToKatakanaConverter : PluginBase, IConverter
+    {
+        public class ParameterKeys
+        {
+            public const string OutputFormat = "OutputFormat";
+        }
+
+        public enum OutputFormat
+        {
+            FullWidth,
+            HalfWidth
+        }
+
+        private static readonly Dictionary<string, string> ConversionMap = new()
+        {
+            {"あ","ア"},
+            {"ぁ","ァ"},
+            {"ぃ","ィ"},
+            {"い","イ"},
+            {"ぅ","ゥ"},
+            {"う","ウ"},
+            {"ぇ","ェ"},
+            {"え","エ"},
+            {"ぉ","ォ"},
+            {"お","オ"},
+            {"か","カ"},
+            {"が","ガ"},
+            {"き","キ"},
+            {"ぎ","ギ"},
+            {"く","ク"},
+            {"ぐ","グ"},
+            {"け","ケ"},
+            {"げ","ゲ"},
+            {"こ","コ"},
+            {"ご","ゴ"},
+            {"さ","サ"},
+            {"ざ","ザ"},
+            {"し","シ"},
+            {"じ","ジ"},
+            {"す","ス"},
+            {"ず","ズ"},
+            {"せ","セ"},
+            {"ぜ","ゼ"},
+            {"そ","ソ"},
+            {"ぞ","ゾ"},
+            {"た","タ"},
+            {"だ","ダ"},
+            {"ち","チ"},
+            {"ぢ","ヂ"},
+            {"っ","ッ"},
+            {"つ","ツ"},
+            {"づ","ヅ"},
+            {"て","テ"},
+            {"で","デ"},
+            {"と","ト"},
+            {"ど","ド"},
+            {"な","ナ"},
+            {"に","ニ"},
+            {"ぬ","ヌ"},
+            {"ね","ネ"},
+            {"の","ノ"},
+            {"は","ハ"},
+            {"ば","バ"},
+            {"ぱ","パ"},
+            {"ひ","ヒ"},
+            {"び","ビ"},
+            {"ぴ","ピ"},
+            {"ふ","フ"},
+            {"ぶ","ブ"},
+            {"ぷ","プ"},
+            {"へ","ヘ"},
+            {"べ","ベ"},
+            {"ぺ","ペ"},
+            {"ほ","ホ"},
+            {"ぼ","ボ"},
+            {"ぽ","ポ"},
+            {"ま","マ"},
+            {"み","ミ"},
+            {"む","ム"},
+            {"め","メ"},
+            {"も","モ"},
+            {"ゃ","ャ"},
+            {"や","ヤ"},
+            {"ゅ","ュ"},
+            {"ゆ","ユ"},
+            {"ょ","ョ"},
+            {"よ","ヨ"},
+            {"ら","ラ"},
+            {"り","リ"},
+            {"る","ル"},
+            {"れ","レ"},
+            {"ろ","ロ"},
+            {"ゎ","ヮ"},
+            {"わ","ワ"},
+            {"ゐ","ヰ"},
+            {"ゑ","ヱ"},
+            {"を","ヲ"},
+            {"ん","ン"},
+            {"う゛", "ヴ" },
+            {"ゔ","ヴ"},
+        };
+
+        private readonly IConverter _wideToNarrowConverter;
+        private readonly PluginParameterCollection _wideToNarrowParameter;
+
+        public HiraganaToKatakanaConverter()
+        {
+            _wideToNarrowConverter = new WideToNarrowConverter();
+            _wideToNarrowParameter = _wideToNarrowConverter.GetParametersSchema();
+
+            foreach (var key in _wideToNarrowParameter.Keys)
+            {
+                if (key == WideToNarrowConverter.ParameterKeys.ConvertKatakana || key == WideToNarrowConverter.ParameterKeys.ConvertSpace)
+                {
+                    _wideToNarrowParameter[key].SetValue(true);
+                }
+                else
+                {
+                    _wideToNarrowParameter[key].SetValue(false);
+                }
+            }
+        }
+
+        public PluginInvocationResult Convert(string input, PluginParameterCollection parameters)
+        {
+            var outputFormat = parameters[ParameterKeys.OutputFormat].GetValue<OutputFormat>();
+
+            var fullWidthKatakana = ConvertToFullWidth(input);
+
+            if (outputFormat == OutputFormat.FullWidth)
+            {
+                return PluginInvocationResult.OfSuccess(fullWidthKatakana);
+            }
+            else
+            {
+                return _wideToNarrowConverter.Convert(fullWidthKatakana, _wideToNarrowParameter);
+            }
+        }
+
+        protected override void ConfigureParameters(PluginParametersConfiguration configuration)
+        {
+            configuration.AddListParameter<OutputFormat>(ParameterKeys.OutputFormat, OutputFormat.FullWidth);
+        }
+
+        private string ConvertToFullWidth(string input)
+        {
+            var sb = new StringBuilder(input);
+            foreach (var kvp in ConversionMap)
+            {
+                sb.Replace(kvp.Key, kvp.Value);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/StarmyKnife.Core/Plugins/BuiltIn/Converters/KatakanaToHiraganaConverter.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarmyKnife.Core.Plugins.BuiltIn.Converters
+{
+    [StarmyKnifePlugin("Katakana to Hiragana")]
+    public class KatakanaToHiraganaConverter : PluginBase, IConverter
+    {
+        private static readonly Dictionary<string, string> ConversionMap = new()
+        {
+            {"ア","あ"},
+            {"ァ","ぁ"},
+            {"ィ","ぃ"},
+            {"イ","い"},
+            {"ゥ","ぅ"},
+            {"ウ","う"},
+            {"ェ","ぇ"},
+            {"エ","え"},
+            {"ォ","ぉ"},
+            {"オ","お"},
+            {"カ","か"},
+            {"ガ","が"},
+            {"キ","き"},
+            {"ギ","ぎ"},
+            {"ク","く"},
+            {"グ","ぐ"},
+            {"ケ","け"},
+            {"ゲ","げ"},
+            {"コ","こ"},
+            {"ゴ","ご"},
+            {"サ","さ"},
+            {"ザ","ざ"},
+            {"シ","し"},
+            {"ジ","じ"},
+            {"ス","す"},
+            {"ズ","ず"},
+            {"セ","せ"},
+            {"ゼ","ぜ"},
+            {"ソ","そ"},
+            {"ゾ","ぞ"},
+            {"タ","た"},
+            {"ダ","だ"},
+            {"チ","ち"},
+            {"ヂ","ぢ"},
+            {"ッ","っ"},
+            {"ツ","つ"},
+            {"ヅ","づ"},
+            {"テ","て"},
+            {"デ","で"},
+            {"ト","と"},
+            {"ド","ど"},
+            {"ナ","な"},
+            {"ニ","に"},
+            {"ヌ","ぬ"},
+            {"ネ","ね"},
+            {"ノ","の"},
+            {"ハ","は"},
+            {"バ","ば"},
+            {"パ","ぱ"},
+            {"ヒ","ひ"},
+            {"ビ","び"},
+            {"ピ","ぴ"},
+            {"フ","ふ"},
+            {"ブ","ぶ"},
+            {"プ","ぷ"},
+            {"ヘ","へ"},
+            {"ベ","べ"},
+            {"ペ","ぺ"},
+            {"ホ","ほ"},
+            {"ボ","ぼ"},
+            {"ポ","ぽ"},
+            {"マ","ま"},
+            {"ミ","み"},
+            {"ム","む"},
+            {"メ","め"},
+            {"モ","も"},
+            {"ャ","ゃ"},
+            {"ヤ","や"},
+            {"ュ","ゅ"},
+            {"ユ","ゆ"},
+            {"ョ","ょ"},
+            {"ヨ","よ"},
+            {"ラ","ら"},
+            {"リ","り"},
+            {"ル","る"},
+            {"レ","れ"},
+            {"ロ","ろ"},
+            {"ヮ","ゎ"},
+            {"ワ","わ"},
+            {"ヰ","ゐ"},
+            {"ヱ","ゑ"},
+            {"ヲ","を"},
+            {"ン","ん"},
+            {"ヴ","ゔ"},
+        };
+
+        private readonly IConverter _narrowToWideConverter;
+        private readonly PluginParameterCollection _narrowToWideParameter;
+
+        public KatakanaToHiraganaConverter()
+        {
+            _narrowToWideConverter = new NarrowToWideConverter();
+            _narrowToWideParameter = _narrowToWideConverter.GetParametersSchema();
+
+            foreach (var key in _narrowToWideParameter.Keys)
+            {
+                if (key == NarrowToWideConverter.ParameterKeys.ConvertKatakana || key == NarrowToWideConverter.ParameterKeys.ConvertSpace)
+                {
+                    _narrowToWideParameter[key].SetValue(true);
+                }
+                else
+                {
+                    _narrowToWideParameter[key].SetValue(false);
+                }
+            }
+        }
+
+        public PluginInvocationResult Convert(string input, PluginParameterCollection parameters)
+        {
+            var wideKatakanaResult = _narrowToWideConverter.Convert(input, _narrowToWideParameter);
+            if (!wideKatakanaResult.Success)
+            {
+                return PluginInvocationResult.OfFailure("Cannot convert narrow katakana to wide");
+            }
+
+            var sb = new StringBuilder(wideKatakanaResult.Value);
+            foreach (var kvp in ConversionMap)
+            {
+                sb.Replace(kvp.Key, kvp.Value);
+            }
+
+            return PluginInvocationResult.OfSuccess(sb.ToString());
+        }
+
+        protected override void ConfigureParameters(PluginParametersConfiguration configuration)
+        {
+            // No parameters
+        }
+    }
+}
